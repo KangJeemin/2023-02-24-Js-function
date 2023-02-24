692392cdd3592de1ea7af7c73cc1dcd932c89fa6
kangjeemin <wlals1234560@naver.com>
2023-02-24 18:00:19 +0900

    함수 연습

--- bbb.js
+++ bbb.js
@@ -1,7 +1,11 @@
 
 function makediv(tag) {
   let root =document.getElementById("root")
-  root.innerHTML+="<"+ "div" + ">"+tag+"</"+"div"+">"
+  root.innerHTML="<"+ "div" + ">"+tag+"</"+"div"+">"
+  root.children[0].innerHTML="<"+ "div" + ">"+"자식"+"</"+"div"+">"
   console.dir(root);
+  
+  
 }
+makediv("부모")
 
